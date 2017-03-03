9d4624fe0d12112240b5c345058f7642276df9d4
nlainez <norman.joya@gmail.com>
2017-03-03 17:52:10 -0600

    enhancing the presentation of products
    adding datatables library in order to have the ability to sort items by name,price, votes(likes)
    also this library enables searching especific data from all the data table and filter it

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,4 @@
-jQuery(function($){
-  $(document).ready(function() {
-    $('#action_buttons').attr('colspan',4);
-    $('#data-table-elvistek-store').DataTable({
-      "pagingType": "full_numbers",
-      "paging": true
-    });
+$(document).ready(function() {
+    $('#action_buttons').attr('colspan', 4);
+    $('#data-table-elvistek-store').dataTable();
   });
-});
